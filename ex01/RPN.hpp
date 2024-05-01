e070d3a2ab67781165671d03e285dd471a939b7c
hcsalmi <101410678+hcsalmi@users.noreply.github.com>
2024-05-01 14:16:47 +0300

    Add files via upload

---
+++ ex01/RPN.hpp
@@ -0,0 +1,23 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <stack>
+#include <iostream>
+
+class RPN
+{
+    private:
+        std::stack<int>  _stack;
+        void    validateOperatePrint(std::string input);
+        void    calculate(char op);
+
+    public:
+        RPN();
+        RPN(const RPN &src);
+        RPN &operator=(const RPN &src);
+        ~RPN();
+
+        void   handleRpn(std::string input);
+};
+
+#endif
